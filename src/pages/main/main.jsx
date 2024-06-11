9f7a41e9dfb85a73b219bef2dd98286dc5fb2455
Abdulatif <157136097+karimoffabdulatif@users.noreply.github.com>
2024-06-11 19:43:10 +0500

    Update main.jsx
    .

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -7,13 +7,6 @@ const Main = () => {
   return (
     <div>
       <ResponsiveDrawer />
-        {/* <Sidebar />
-        <div className="main-left">
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-        </div> */}
     </div>
   )
 }
